cecc9d146a87c5a6201e9ae1385981c57650f1fa
Louise Jones <louisejones94@live.co.uk>
2023-04-28 11:08:01 +0100

    Root route

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+// Require necessary NPM packages
+const express = require('express')
+
+// Instantiate a router (a mini app that only handles routes)
+const router = express.Router()
+
+/*
+* Action: INDEX (show all the information)
+* Method: GET
+* URI: /
+* Description: Root route - Greet users to the app
+*/
+router.get('/', (req,res) => {
+    res.json({message: 'Welcome to Bloggy'})
+})
+
+// Export the router so we can use it in the server.js file
+module.exports = router
--- server.js
+++ server.js
@@ -2,12 +2,19 @@
 const express = require('express')
 const mongoose = require ('mongoose')
 
+// Require necessary route files
+const indexRouter = require('./routes/index')
+
 // Instantiate express application object
 const app = express()
 
 // Define port for the API to run on
 // Try to find an environment first, if not then go for 5000
 const port = process.env.PORT || 5004
+
+// Mount the required routes (like a middleware)
+app.use(indexRouter)
+
 
 // Start the server and listen for requests on the given port
 app.listen(port, () => console.log(`bloggy is listening on port ${port}`))
