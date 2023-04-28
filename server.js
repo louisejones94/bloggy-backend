0df055afc224cab1b8a4eef6ebffa8b7e39b471e
Louise Jones <louisejones94@live.co.uk>
2023-04-28 11:29:05 +0100

    Configure MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 // Require necessary NPM packages
 const express = require('express')
 const mongoose = require ('mongoose')
+const db = mongoose.connection
+
+// Require DB configuration file
+const dbConfig = require ('./config/db')
+
+// Establish database connection
+mongoose.connect(dbConfig)
+db.on('error', (error) => console.log(`ERROR: ${error.message}`))
+db.on('connected', () => console.log(`MongoDB Connected: ${dbConfig}`))
+db.on('disconnected', () => console.log('MongoDB Disconnected'))
 
 // Require necessary route files
 const indexRouter = require('./routes/index')
